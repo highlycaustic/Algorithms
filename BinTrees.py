4f01cd27b6c3a15938421c78a16a8ee070c193c8
hcl_laptop <hcl@laptop>
2025-06-17 11:39:00 +0900

    Optional arguments in TreeNode constructor, functions to print and put tree into list

--- BinTrees.py
+++ BinTrees.py
@@ -1,8 +1,22 @@
 class TreeNode:
-    def __init__(self, data):
+    def __init__(self, data, left = None, right = None):
         self.data  = data
         self.left  = None
         self.right = None
+        self.left  = left
+        self.right = right
+
+def tree_into_list(node, _list):
+    if node is not None:
+        tree_into_list(node.left, _list)
+        _list.append(node.data)
+        tree_into_list(node.right, _list)
+
+def print_tree(node, level=0):
+    if node is not None:
+        print_tree(node.right, level + 1)
+        print(' ' * 4 * level + '-> ' + str(node.data))
+        print_tree(node.left, level + 1)
 
 def node_lnr(TreeNode):
     if TreeNode is None:
